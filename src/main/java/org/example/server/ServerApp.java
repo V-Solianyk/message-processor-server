3bd4e797f2af4638bcb4a14f37aef1a8127ce108
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-11-04 20:26:56 +0200

    fixed bugs in the ServerHandler and added some functionality.

--- src/main/java/org/example/server/ServerApp.java
+++ src/main/java/org/example/server/ServerApp.java
@@ -3,21 +3,35 @@ package org.example.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.example.server.handler.ServerHandler;
 
 public class ServerApp {
+    private static final Map<Socket, Integer> clientCounters = new ConcurrentHashMap<>();
+    private static final int PORT = 3344;
+
     public static void main(String[] args) {
-        int port = 8888;
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server started. Waiting for client...");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 ServerHandler serverHandler = new ServerHandler(clientSocket);
+                clientCounters.put(clientSocket, 0);
                 serverHandler.run();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("An error occurred while running the server or accepting" +
+                    " client connections: " + e.getMessage(), e);
         }
+    }
+
+    public static int getClientCounter(Socket clientSocket) {
+        return clientCounters.get(clientSocket);
+    }
+
+    public static void incrementClientCounter(Socket clientSocket) {
+        clientCounters.computeIfPresent(clientSocket, (key, value) -> value + 1);
     }
 }
--- src/main/java/org/example/server/handler/ServerHandler.java
+++ src/main/java/org/example/server/handler/ServerHandler.java
@@ -7,8 +7,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.example.server.ServerApp;
 
 public class ServerHandler implements Runnable {
+    private static final String FILE_NAME_FOR_COMMAND = "Commands.txt";
+    private static final String FILE_NAME_FOR_LOGGING = "LogFile.txt";
+    private static final String ALPHABETIC_PATTERN = ".*[a-zA-Z]+.*";
+    private static final String NUMERIC_PATTERN = "\\d+";
+    private static final int MULTIPLIER = 1000;
+    private static final int COUNT_OF_THREADS = 1;
+    private static final int PERIOD = 10;
+
     private final Socket socket;
 
     public ServerHandler(Socket socket) {
@@ -17,9 +31,15 @@ public class ServerHandler implements Runnable {
 
     @Override
     public void run() {
-        try {
-            BufferedReader inputFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter outputToClient = new PrintWriter(socket.getOutputStream(), true);
+        ScheduledExecutorService scheduler = null;
+        try (BufferedReader inputFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter outputToClient = new PrintWriter(socket.getOutputStream(), true)) {
+            scheduler = Executors.newScheduledThreadPool(COUNT_OF_THREADS);
+            scheduler.scheduleAtFixedRate(() -> {
+                ServerApp.incrementClientCounter(socket);
+                int counter = ServerApp.getClientCounter(socket);
+                sendPeriodicMessage(counter);
+            }, 0, PERIOD, TimeUnit.SECONDS);
 
             String input;
             while ((input = inputFromClient.readLine()) != null) {
@@ -31,17 +51,22 @@ public class ServerHandler implements Runnable {
                 writeToLogFile("Received: " + input, "Server response: " + response);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("An error occurred while handling client's request: "
+                    + e.getMessage(), e);
+
+        } finally {
+            if (scheduler != null) {
+                scheduler.shutdown();
+            }
         }
     }
-
 
     private String processClientInput(String input) {
         String command = processCommand(input);
         if (command.equals("Command not found")) {
-            if (input.matches(".*[a-zA-Z]+.*")) {
+            if (input.matches(ALPHABETIC_PATTERN)) {
                 return modifyString(input);
-            } else if (input.matches("\\d+")) {
+            } else if (input.matches(NUMERIC_PATTERN)) {
                 return multiplyByThousand(input);
             }
         }
@@ -70,11 +95,11 @@ public class ServerHandler implements Runnable {
 
     private String multiplyByThousand(String message) {
         int number = Integer.parseInt(message);
-        return String.valueOf(number * 1000);
+        return String.valueOf(number * MULTIPLIER);
     }
 
     private String processCommand(String command) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("Commands.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME_FOR_COMMAND))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(":");
@@ -83,16 +108,39 @@ public class ServerHandler implements Runnable {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("An error occurred while trying to read from a file: "
+                    + FILE_NAME_FOR_COMMAND, e);
         }
         return "Command not found";
     }
 
     private void writeToLogFile(String received, String sent) {
-        try (FileWriter writer = new FileWriter("LogFile.txt", true)) {
+        try (FileWriter writer = new FileWriter(FILE_NAME_FOR_LOGGING, true)) {
             writer.write(received + ", " + sent + "\n");
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Can't write the message to the logFile: "
+                    + FILE_NAME_FOR_LOGGING, e);
+        }
+    }
+
+    private void writePeriodicMessageToLogFile(String sent) {
+        try (FileWriter writer = new FileWriter(FILE_NAME_FOR_LOGGING, true)) {
+            writer.write(sent + "\n");
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write the periodic message to the logFile: "
+                    + FILE_NAME_FOR_LOGGING, e);
+        }
+    }
+
+    private void sendPeriodicMessage(int counter) {
+        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+        String message = "Counter " + counter + ", Time " + currentTime;
+        try (PrintWriter outputToClient = new PrintWriter(socket.getOutputStream(), true)) {
+//            outputToClient.println(message);
+            writePeriodicMessageToLogFile(message);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to send periodic message to the client. Please "
+                    + "check the connection and try again.", e);
         }
     }
 }
