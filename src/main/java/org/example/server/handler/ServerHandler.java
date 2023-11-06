5deab4c3bf94451db88a3dbfc8a371e098c2751b
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-11-06 17:11:36 +0200

    added log4j for server

--- src/main/java/org/example/server/handler/ServerHandler.java
+++ src/main/java/org/example/server/handler/ServerHandler.java
@@ -1,19 +1,22 @@
 package org.example.server.handler;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ServerHandler {
     private static final String FILE_NAME_FOR_COMMAND = "Commands.txt";
-    private static final String FILE_NAME_FOR_LOGGING = "LogFile.txt";
     private static final String ALPHABETIC_PATTERN = ".*[a-zA-Z]+.*";
     private static final String NUMERIC_PATTERN = "\\d+";
     private static final int MULTIPLIER = 1000;
+    private static final Logger LOGGER = LogManager.getLogger(ServerHandler.class.getName());
 
     private final Socket socket;
 
@@ -22,18 +25,17 @@ public class ServerHandler {
     }
 
     public void run() {
-        try (BufferedReader inputFromClient = new BufferedReader(new InputStreamReader(socket
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket
                 .getInputStream()));
              PrintWriter outputToClient = new PrintWriter(socket.getOutputStream(), true)) {
             String input;
-                while ((input = inputFromClient.readLine()) != null) {
-                    System.out.println("Received from client: " + input);
-                    String response = processClientInput(input);
-                    outputToClient.println(response);
-                    writeToLogFile("Received: " + input, "Server answer: " + response);
-                }
+            while ((input = reader.readLine()) != null) {
+                String response = processClientInput(input);
+                outputToClient.println(response);
+                LOGGER.info("Received: " + input + ", Server answer: " + response);
+            }
         } catch (IOException e) {
-            System.out.println("The client finished connect with the server");
+            LOGGER.error("The client finished connect with the server");
         }
     }
 
@@ -46,7 +48,7 @@ public class ServerHandler {
                 return multiplyByThousand(input);
             }
         }
-        return command;
+        return "Server answer: " + command;
     }
 
     private String modifyString(String message) {
@@ -66,36 +68,27 @@ public class ServerHandler {
                 result.append(c);
             }
         }
-        return result.toString();
+        return "Server answer: " + result;
     }
 
     private String multiplyByThousand(String message) {
         int number = Integer.parseInt(message);
-        return String.valueOf(number * MULTIPLIER);
+        return "Server answer: " + (number * MULTIPLIER);
     }
 
-    private String processCommand(String command) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME_FOR_COMMAND))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(":");
-                if (parts[0].equals(command)) {
+    private String processCommand(String input) {
+        try {
+            List<String> commands = Files.readAllLines(Path.of(FILE_NAME_FOR_COMMAND));
+            for (String command : commands) {
+                String[] parts = command.split(":");
+                if (parts[0].equals(input)) {
                     return parts[1];
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException("An error occurred while trying to read from a file: "
+            LOGGER.error("An error occurred while trying to read from a file: "
                     + FILE_NAME_FOR_COMMAND, e);
         }
         return "Command not found";
-    }
-
-    private void writeToLogFile(String received, String sent) {
-        try (FileWriter writer = new FileWriter(FILE_NAME_FOR_LOGGING, true)) {
-            writer.write(received + ", " + sent + "\n");
-        } catch (IOException e) {
-            throw new RuntimeException("Can't write the message to the logFile: "
-                    + FILE_NAME_FOR_LOGGING, e);
-        }
     }
 }
